55adf8a799ad14e6b09c6783383b2d40eabff80a
Luis carlos <luisxhl7@gmail.com>
2024-03-06 23:10:15 -0500

    adding folders

---
+++ src/assets/image.js
@@ -0,0 +1,41 @@
+import git from './git.svg'
+import github from './github.svg'
+import javaScript from './javaScript.svg'
+import typeScript from './typescript.svg'
+import react from './react.svg'
+import html from './html.svg'
+import sass from './sass.svg'
+import css from './css.svg'
+import awsImage from './aws_image.svg'
+import testing_library from './testing-library.svg'
+import vista_my_music_app from './vista-my-music-app.svg'
+import vista_contact_x from './vista-Contact-X.svg'
+import vista_my_pokedex from './My-Pokedex.svg'
+import vista_my_portafolio from './my-portafolio.svg'
+import vista_my_store from './My-store.svg'
+import vista_my_budget from './My-budget.svg'
+import bootstrap from './bootstrap.svg'
+import materialUI from './material-UI.svg'
+import nextJs from './nextJs.png'
+
+export const images = {
+    git,
+    github,
+    javaScript,
+    typeScript,
+    react,
+    sass,
+    testing_library,
+    vista_my_music_app,
+    vista_contact_x,
+    vista_my_portafolio,
+    vista_my_store,
+    html,
+    css,
+    vista_my_pokedex,
+    awsImage,
+    vista_my_budget,
+    bootstrap,
+    materialUI,
+    nextJs
+}
